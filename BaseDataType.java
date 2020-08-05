8f58bae20e3fef05a73614586fb8302c3126169e
jackkitte <jackkitte735@gmail.com>
2020-08-06 08:15:34 +0900

    テータ型

---
+++ BaseDataType.java
@@ -0,0 +1,29 @@
+public class BaseDataType {
+
+  public static void main(String[] args) {
+    byte a1 = 1;
+    short a2 = 10;
+    int a3 = 100;
+    long a4 = 1000;
+
+    System.out.println(a1);
+    System.out.println(a2);
+    System.out.println(a3);
+    System.out.println(a4);
+
+    double b1 = 10.1;
+    float b2 = 12.2f;
+
+    System.out.println(b1);
+    System.out.println(b2);
+
+    char c1 = 'あ';
+    System.out.println(c1);
+
+    boolean d1 = true;
+    boolean d2 = false;
+
+    System.out.println(d1);
+    System.out.println(d2);
+  }
+}
---
+++ FinalVariable.java
@@ -0,0 +1,27 @@
+public class FinalVariable {
+
+  public static void main(String[] args) {
+
+    final int UP = 0;
+    final int DOWN = 1;
+
+    System.out.println(UP);
+    System.out.println(DOWN);
+
+    final float PI = 3.14f;
+
+    System.out.println(PI);
+
+    final boolean RUN = true;
+
+    System.out.println(RUN);
+
+    final char A = 'A';
+
+    System.out.println(A);
+
+    final String CONST = "love for forever";
+
+    System.out.println(CONST);
+  }
+}
