a873928fbf34945114364474efe93f9aefeae6d4
jackkitte <jackkitte735@gmail.com>
2020-08-10 14:13:13 +0900

    canvasの作成

---
+++ canvas/ProgressBar.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
+class Canvas extends JPanel {
+  public Canvas() {
+    this.setLayout(null);
+    this.setBackground(Color.WHITE);
+  }
+
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+  }
+}
+
+public class ProgressBar {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("GUI Canvas Application");
+    Canvas canvas = new Canvas();
+    frame.add(canvas);
+    frame.setSize(300, 200);
+    frame.setVisible(true);
+  }
+}
