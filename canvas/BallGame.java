ed9be2cb1d052d4c6b1ac69cbf2447a905575eca
jackkitte <jackkitte735@gmail.com>
2020-08-10 15:02:26 +0900

    Ballクラスからボールを描画

--- canvas/BallGame.java
+++ canvas/BallGame.java
@@ -1,6 +1,13 @@
 import java.awt.*;
 import javax.swing.*;
 
+class Ball {
+  public void draw(Graphics g, int x, int y, int r, Color c) {
+    g.setColor(c);
+    g.fillOval(x, y, 2 * r, 2 * r);
+  }
+}
+
 class Canvas extends JPanel {
   public Canvas() {
     this.setLayout(null);
@@ -9,8 +16,10 @@ class Canvas extends JPanel {
 
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
-    g.setColor(Color.BLUE);
-    g.fillOval(80, 80, 100, 100);
+    Ball ball = new Ball();
+    ball.draw(g, 50, 50, 30, Color.RED);
+    ball.draw(g, 120, 50, 30, Color.GREEN);
+    ball.draw(g, 190, 50, 30, Color.BLUE);
   }
 }
 
