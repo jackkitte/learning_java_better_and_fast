3919e747245c6d084a696fb8dbbd8bd753989db0
jackkitte <jackkitte735@gmail.com>
2020-08-10 15:39:50 +0900

    ボールを上下交互に動かす

--- canvas/BallGame.java
+++ canvas/BallGame.java
@@ -24,11 +24,26 @@ class Ball {
   public void move(int distance) {
     this.y = this.y + distance;
   }
+
+  public int getX() {
+    return this.x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public int getR() {
+    return this.r;
+  }
 }
 
 class Canvas extends JPanel {
   protected Ball ball;
   private boolean isRun = true;
+  private int canvasWidth = 200;
+  private int canvasHeight = 300;
+
   CanvasThread CanvasThread;
 
   public Canvas() {
@@ -41,8 +56,10 @@ class Canvas extends JPanel {
   }
 
   protected void paintComponent(Graphics g) {
+    canvasWidth = this.getWidth();
+    canvasHeight = this.getHeight();
     super.paintComponent(g);
-    g.clearRect(0, 0, 200, 300);
+    g.clearRect(0, 0, canvasWidth, canvasHeight);
 
     ball.draw(g);
   }
@@ -50,11 +67,22 @@ class Canvas extends JPanel {
   class CanvasThread extends Thread {
     @Override
     public void run() {
+      String direction = "down";
       while (isRun) {
         try {
-          ball.move(10);
-          Thread.sleep(200);
+          if (ball.getY() + 2 * ball.getR() <= canvasHeight && direction.equals("down")) {
+            ball.move(10);
+          } else {
+            direction = "up";
+          }
+
+          if (ball.getY() >= 0 && direction.equals("up")) {
+            ball.move(-10);
+          } else {
+            direction = "down";
+          }
           Canvas.this.repaint();
+          Thread.sleep(50);
         } catch (InterruptedException e) {
           e.printStackTrace();
         }
