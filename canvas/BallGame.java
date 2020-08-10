80274777fa6a54625a300fad9feefe96057c3bd6
jackkitte <jackkitte735@gmail.com>
2020-08-10 15:16:01 +0900

    ボールを下へ落とす動作

--- canvas/BallGame.java
+++ canvas/BallGame.java
@@ -2,24 +2,64 @@ import java.awt.*;
 import javax.swing.*;
 
 class Ball {
-  public void draw(Graphics g, int x, int y, int r, Color c) {
-    g.setColor(c);
-    g.fillOval(x, y, 2 * r, 2 * r);
+  private int x;
+  private int y;
+  private int r;
+  private Color color;
+
+  public Ball(int x, int y, int r, Color color) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.color = color;
+  }
+
+  public void draw(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g;
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g2.setColor(this.color);
+    g2.fillOval(this.x, this.y, 2 * this.r, 2 * this.r);
+  }
+
+  public void move(int distance) {
+    this.y = this.y + distance;
   }
 }
 
 class Canvas extends JPanel {
+  protected Ball ball;
+  private boolean isRun = true;
+  CanvasThread CanvasThread;
+
   public Canvas() {
     this.setLayout(null);
     this.setBackground(Color.WHITE);
+
+    ball = new Ball(50, 50, 30, Color.BLUE);
+    CanvasThread = new CanvasThread();
+    CanvasThread.start();
   }
 
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
-    Ball ball = new Ball();
-    ball.draw(g, 50, 50, 30, Color.RED);
-    ball.draw(g, 120, 50, 30, Color.GREEN);
-    ball.draw(g, 190, 50, 30, Color.BLUE);
+    g.clearRect(0, 0, 200, 300);
+
+    ball.draw(g);
+  }
+
+  class CanvasThread extends Thread {
+    @Override
+    public void run() {
+      while (isRun) {
+        try {
+          ball.move(10);
+          Thread.sleep(200);
+          Canvas.this.repaint();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
   }
 }
 
@@ -28,7 +68,7 @@ public class BallGame {
     JFrame frame = new JFrame("GUI Canvas Application");
     Canvas canvas = new Canvas();
     frame.add(canvas);
-    frame.setSize(300, 300);
+    frame.setSize(200, 300);
     frame.setVisible(true);
   }
 }
